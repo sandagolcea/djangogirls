09f3e8b847492c0935d14f7f9dff21dc1733ae07
Sanda Golcea <sanda.golcea@gmail.com>
2023-07-29 15:30:13 +0300

    Retrieves posts from DB and shows them in the html view

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-# Create your models here.
 
+# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
